5272859d6a6076e8324f20541a4388e6ccae8f95
Danny1G <154330210+Danny1G@users.noreply.github.com>
2023-12-19 19:46:31 +0800

    Add files via upload
    BSIT1G ANDAL

---
+++ FINALS1G/register.js
@@ -0,0 +1,16 @@
+function getUsername() {
+    
+    var username = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    
+    if (username === "" || email === "" || password === "") {
+        document.getElementById("result").innerHTML = "Please fill in all fields.";
+    } else {
+        
+        document.getElementById("result").innerHTML = "Register successful! Welcome, " + username + "!";
+        
+        window.location.href = 'Login.html';
+    }
+}
